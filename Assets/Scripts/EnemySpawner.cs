45afe09c68cf8eb82ec4be02fda6f9b352464ca1
Joaquin Sanz Pizia <joaquin@pizia.com>
2023-06-06 17:39:40 -0300

    Changed layout to portrait, added some UI

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,6 +6,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] public List<GameObject> allEnemies = new List<GameObject>();
     [SerializeField] LeanTweenPath enemyPath;
+    [SerializeField] float travelTime;
     [SerializeField] float spawnDelay;
     [SerializeField] int slimeAmount;
 
@@ -22,7 +23,7 @@ public class EnemySpawner : MonoBehaviour
         GameObject newEnemy = pooler.SpawnFromPool($"{randomSlime}", transform.position + new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0f), transform.rotation);
         allEnemies.Add(newEnemy);
 
-        LeanTween.move(newEnemy, enemyPath.vec3, 20f).setOnComplete(() => 
+        LeanTween.move(newEnemy, enemyPath.vec3, travelTime).setOnComplete(() => 
         {
             newEnemy.GetComponent<Enemy>().Die();
         });
